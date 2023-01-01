f8504dda67a7ac6672aa436be3e70eb21b696d0b
Ferdinand Bauer <ferdinand.bauer@scitotec.de>
2023-01-01 21:54:59 +0100

    feat: first prototype

---
+++ src/slides/longdrinks.jsx
@@ -0,0 +1,16 @@
+export function Longdrinks() {
+    return (<div>
+                <h1>Longdrinks</h1>
+
+                <table>
+                    <tbody>
+                        <tr>
+                            <td>Gin Tonic Thomas Henry</td>
+                            <td>Havanna Cola</td>
+                        </tr>
+                    </tbody>
+                </table>
+
+            </div>
+    )
+}
