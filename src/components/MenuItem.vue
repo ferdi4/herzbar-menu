b990aa4e0515b4e102cbee8707ed044d254f6192
Ferdinand Bauer <f.bau3r@gmail.com>
2023-12-25 12:34:20 +0100

    menu item component

---
+++ src/components/MenuItem.vue
@@ -0,0 +1,16 @@
+<template>
+  <div v-if="group">
+    <h2>{{ group.name }}</h2>
+    <div class="q-mt-md" v-for="item in group.items" :key="item.name">
+      {{ item.name }}
+    </div>
+  </div>
+</template>
+
+<script setup lang="ts">
+import { Group } from './models'
+
+defineProps<{
+  group?: Group
+}>()
+</script>
--- src/pages/MenuPage.vue
+++ src/pages/MenuPage.vue
@@ -16,10 +16,7 @@
       <q-carousel-slide :name="group.name" class="q-pa-none" v-for="group in menu" :key="group.name">
         <q-scroll-area class="fit">
           <div class="column no-wrap flex-center q-carousel--padding">
-            {{ group.name }}
-            <div class="q-mt-md"  v-for="item in group.items" :key="item.name">
-              {{ item.name }}
-            </div>
+           <menu-item :group="group" />
           </div>
         </q-scroll-area>
       </q-carousel-slide>
@@ -31,6 +28,7 @@
 import { ref } from 'vue'
 import { Group } from 'src/components/models';
 import data from '../assets/menu.json'
+import MenuItem from 'src/components/MenuItem.vue';
 
 const slide = ref('Heissgetränke')
 const menu: Group[] = data;
