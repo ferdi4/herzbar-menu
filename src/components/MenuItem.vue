e057e9a4c746c6daab79fcf6522bc4544f914d28
Ferdinand Bauer <ferdinand.bauer@scitotec.de>
2024-01-04 13:31:24 +0100

    feat: adjust layout - set primary and secondary colors - rework volume and price layout

--- src/components/MenuItem.vue
+++ src/components/MenuItem.vue
@@ -1,22 +1,27 @@
 <template>
     <div v-if="group">
-        <h4>{{ group.name }}</h4>
+        <h4 class="text-primary">{{ group.name }}</h4>
         <p>{{ group.description }}</p>
 
         <q-list separator>
             <q-item v-for="item in group.items" :key="item.name" class="q-pl-none">
                 <q-item-section>
                     <q-item-label>
-                        {{ item.name }}
+                      <span class="text-primary">{{ item.name }}</span>
                     </q-item-label>
-                    <q-item-label caption>{{ item?.description }}</q-item-label>
+                    <q-item-label caption v-if="item?.description">
+                      <span class="text-secondary">{{ item.description }}</span>
+                    </q-item-label>
                 </q-item-section>
                 <q-item-section side>
-                    <div v-for="variant in item.variants" :key="variant.name">
-                        <span v-if="variant?.name">{{
-                            variant.name
-                            }} | </span><span>{{ moneyFormatter.format(variant.price) }}</span>
-                    </div>
+                  <table>
+                    <tbody>
+                      <tr v-for="variant in item.variants" :key="variant.name">
+                        <td class="text-right q-pr-md text-secondary">{{variant.name}}</td>
+                        <td v-if="variant?.price" class="text-right text-primary">{{ moneyFormatter.format(variant.price) }}</td>
+                      </tr>
+                    </tbody>
+                  </table>
                 </q-item-section>
             </q-item>
         </q-list>
